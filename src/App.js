d706d722602c421f39010f885aa2c94c4350bd7e
seifeddine bourkhis <seifeddinebourkhis11@gmail.com>
2022-02-15 11:20:00 +0100

    reactjscheckpoint

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import SimpleForm from "./components/SimpleForm";
+
+function App() {
+    return (
+      <div className='App'>
+        <SimpleForm />
+      </div>
+    );   
+}
+export default App;
